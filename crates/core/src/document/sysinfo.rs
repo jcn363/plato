31304fac23d13094d582ef45047d31ee615da7ca
jcn363 <jcn363@gmail.com>
2026-04-25 04:28:39 +0200

    Remove incorrect #[allow(dead_code)] from INTERNAL_CARD_ROOT
    - INTERNAL_CARD_ROOT is used in multiple places (consts.rs, settings/defaults.rs, settings/mod.rs)
    - The dead_code allowance was incorrect and has been removed

--- crates/core/src/document/sysinfo.rs
+++ crates/core/src/document/sysinfo.rs
@@ -17,7 +17,6 @@ use std::env;
 use std::fs;
 use std::process::Command;
 
-#[allow(dead_code)]
 const INTERNAL_CARD_ROOT: &str = "/mnt/onboard";
 
 const CPUINFO_KEYS: [&str; 3] = ["Processor", "Features", "Hardware"];
